634fff5408084d36e5f8267ba17ae087f2e4f27c
Cuber01 <johnmallard631@gmail.com>
2022-04-04 20:29:58 +0200

    Cleanup

---
+++ include/pretty_printer.h
@@ -0,0 +1,8 @@
+#ifndef WALLY_PRETTY_PRINTER_H
+#define WALLY_PRETTY_PRINTER_H
+
+#include "scanner.h"
+
+void printToken(Token token, bool isNewLine);
+
+#endif //WALLY_PRETTY_PRINTER_H
--- src/compiler.c
+++ src/compiler.c
@@ -1,56 +1,6 @@
-#include <stdio.h>
-
-#include "common.h"
-#include "colors.h"
 #include "compiler.h"
 #include "scanner.h"
-
-char* tokenEnumToChar(TokenType type)
-{
-    switch (type)
-    {
-        case TOKEN_LEFT_BRACE: return "TOKEN_LEFT_BRACE";
-        case TOKEN_RIGHT_BRACE: return "TOKEN_RIGHT_BRACE";
-        case TOKEN_COMMA: return "TOKEN_COMMA";
-        case TOKEN_DOT: return "TOKEN_DOT";
-        case TOKEN_MINUS: return "TOKEN_MINUS";
-        case TOKEN_PLUS: return "TOKEN_PLUS";
-        case TOKEN_SEMICOLON: return "TOKEN_SEMICOLON";
-        case TOKEN_SLASH: return "TOKEN_SLASH";
-        case TOKEN_STAR: return "TOKEN_STAR";
-        case TOKEN_BANG: return "TOKEN_BANG";
-        case TOKEN_BANG_EQUAL: return "TOKEN_BANG_EQUAL";
-        case TOKEN_EQUAL: return "TOKEN_EQUAL";
-        case TOKEN_EQUAL_EQUAL: return "TOKEN_EQUAL_EQUAL";
-        case TOKEN_GREATER: return "TOKEN_GREATER";
-        case TOKEN_GREATER_EQUAL: return "TOKEN_GREATER_EQUAL";
-        case TOKEN_LESS_EQUAL: return "TOKEN_LESS_EQUAL";
-        case TOKEN_IDENTIFIER: return "TOKEN_IDENTIFIER";
-        case TOKEN_STRING: return "TOKEN_STRING";
-        case TOKEN_NUMBER: return "TOKEN_NUMBER";
-        case TOKEN_AND: return "TOKEN_AND";
-        case TOKEN_CLASS: return "TOKEN_CLASS";
-        case TOKEN_ELSE: return "TOKEN_ELSE";
-        case TOKEN_FALSE: return "TOKEN_FALSE";
-        case TOKEN_FOR: return "TOKEN_FOR";
-        case TOKEN_FUN: return "TOKEN_FUN";
-        case TOKEN_IF: return "TOKEN_IF";
-        case TOKEN_NIL: return "TOKEN_NIL";
-        case TOKEN_OR: return "TOKEN_OR";
-        case TOKEN_PRINT: return "TOKEN_PRINT";
-        case TOKEN_RETURN: return "TOKEN_RETURN";
-        case TOKEN_SUPER: return "TOKEN_SUPER";
-        case TOKEN_THIS: return "TOKEN_THIS";
-        case TOKEN_TRUE: return "TOKEN_TRUE";
-        case TOKEN_VAR: return "TOKEN_VAR";
-        case TOKEN_WHILE: return "TOKEN_WHILE";
-        case TOKEN_ERROR: return "TOKEN_ERROR";
-        case TOKEN_EOF: return "TOKEN_EOF";
-        case TOKEN_LEFT_PAREN: return "TOKEN_LEFT_PAREN";
-        case TOKEN_RIGHT_PAREN: return "TOKEN_RIGHT_PAREN";
-        case TOKEN_LESS: return "TOKEN_LESS";
-    }
-}
+#include "pretty_printer.h"
 
 void compile(const char* source)
 {
@@ -60,29 +10,15 @@ void compile(const char* source)
     for (;;)
     {
         Token token = scanToken();
+        bool isNewLine = false;
 
         if (token.line != line)
         {
-            printf("%4d ", token.line);
             line = token.line;
-        } else
-        {
-            printf("   | ");
+            isNewLine = true;
         }
 
-        char* text = tokenEnumToChar(token.type);
-        // 19 is longest token's length (TOKEN_GREATER_EQUAL)
-        uint8_t spacesToPrint = 19 - strlen(text);
-
-        printf(GREEN);
-        printf("%s  ", text);
-        while (spacesToPrint--)
-        {
-            putchar(' ');
-        }
-        printf(WHITE);
-
-        printf("'%.*s'\n", token.length, token.start);
+        printToken(token, isNewLine);
 
         if (token.type == TOKEN_EOF) break;
     }
