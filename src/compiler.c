33d97ea60c7ba9cc65247c0fc23e4c67733ecb3f
Cuber01 <johnmallard631@gmail.com>
2022-04-14 09:18:32 +0200

    Add function parameters

--- src/compiler.c
+++ src/compiler.c
@@ -796,6 +796,20 @@ static void function(FunctionType type)
     beginScope();
 
     consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+
+    if (!check(TOKEN_RIGHT_PAREN))
+    {
+        do {
+            current->function->arity++;
+            if (current->function->arity > 255)
+            {
+                errorAtCurrent("Can't have more than 255 parameters.");
+            }
+            uint8_t constant = parseVariable("Expect parameter name.");
+            defineVariable(constant);
+        } while (match(TOKEN_COMMA));
+    }
+
     consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
     consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
     block();
@@ -858,6 +872,12 @@ static void initCompiler(Compiler* compiler, FunctionType type)
     compiler->function = newFunction();
     current = compiler;
 
+    if (type != TYPE_SCRIPT)
+    {
+        current->function->name = copyString(parser.previous.start,
+                                             parser.previous.length);
+    }
+
     Local* local = &current->locals[current->localCount++];
     local->depth = 0;
     local->name.start = "";
@@ -905,7 +925,6 @@ ObjFunction* compile(const char* source)
         declaration();
     }
 
-    endCompiler();
     consume(TOKEN_EOF, "Expect end of expression.");
 
     ObjFunction* function = endCompiler();
