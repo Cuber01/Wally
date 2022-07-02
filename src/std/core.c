6e4a23237b52b58b9eff023c174c7a13c2cb4512
Cuber01 <johnmallard631@gmail.com>
2022-07-02 09:32:04 +0200

    Add type() native function

--- src/std/core.c
+++ src/std/core.c
@@ -1,6 +1,14 @@
 #include "core.h"
 #include "native_utils.h"
 
+ObjString* boolStringConst;
+ObjString* nullStringConst;
+ObjString* numberStringConst;
+ObjString* functionStringConst;
+ObjString* classStringConst;
+ObjString* instanceStringConst;
+ObjString* stringStringConst;
+
 NATIVE_FUNCTION(print)
 {
     printRawValue(args[0]);
@@ -10,12 +18,54 @@ NATIVE_FUNCTION(print)
 
 NATIVE_FUNCTION(type)
 {
-    printRawValue(args[0]);
-    putchar('\n');
-    return NULL_VAL;
+    Value value = args[0];
+
+    if (IS_BOOL(value))
+    {
+        return OBJ_VAL((Obj*)boolStringConst);
+    }
+    else if (IS_NULL(value))
+    {
+        return OBJ_VAL((Obj*)nullStringConst);
+    }
+    else if (IS_NUMBER(value))
+    {
+        return OBJ_VAL((Obj*)numberStringConst);
+    }
+    else if (IS_STRING(value))
+    {
+        return OBJ_VAL((Obj*)stringStringConst);
+    }
+    else if (IS_INSTANCE(value))
+    {
+        return OBJ_VAL((Obj*)instanceStringConst);
+    }
+    else if (IS_CLASS(value))
+    {
+        return OBJ_VAL((Obj*)classStringConst);
+    }
+    else if (IS_FUNCTION(value) || IS_BOUND_METHOD(value) || IS_NATIVE(value))
+    {
+        return OBJ_VAL((Obj*)functionStringConst);
+    }
+    else
+    {
+        printf("Unreachable reached");
+        return NULL_VAL;
+    }
+
 }
 
 void defineCore(Table* table)
 {
     defineNativeFunction(table, "print", printNative);
+    defineNativeFunction(table, "type", typeNative);
+
+    boolStringConst = copyString("bool", 4);
+    nullStringConst = copyString("null", 4);
+    numberStringConst = copyString("string", 6);
+    functionStringConst = copyString("function", 8);
+    classStringConst = copyString("class", 5);
+    instanceStringConst = copyString("instance", 8);
+    stringStringConst = copyString("string", 5);
 }
